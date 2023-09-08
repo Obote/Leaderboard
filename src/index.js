056298d1c0d574aee3e6d90e2067fb69dbad65c4
Obote <obotedenis256@gmail.com>
2023-09-08 12:51:35 +0300

    Create sample leaderboard data

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scoreList = document.querySelector('.score-list');
+
+  let LeaderBoardData = [
+    {name: 'Manchester City', score: 12},
+    {name: 'Tottenham Hotspur', score: 10},
+    {name: 'Liverpool', score: 10},
+    {name: 'Westhan United', score: 10}
+  ];
+
+
+
+})
